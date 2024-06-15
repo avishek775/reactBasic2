6f9030ea636e149b4781fc15958d3f8949f78c55
abhishek singh <abhishekkumarsingh276642@gmail.com>
2024-06-15 11:16:25 +0530

    first commit

---
+++ 05card/src/Card.jsx
@@ -0,0 +1,11 @@
+function  Card() {
+    return(
+     <div className="card">
+        <img className="card-image" src="https://images.pexels.com/photos/23017731/pexels-photo-23017731/free-photo-of-a-man-in-a-yellow-jacket-standing-in-the-woods.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="profile " />
+        <h2 className="card-title">Chai aur Sutta</h2>
+        <p className="card-text">My Name Is Gagan singh</p>
+
+     </div>
+    )
+}
+export default Card
